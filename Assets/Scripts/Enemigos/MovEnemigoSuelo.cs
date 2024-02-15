944339397bac7dd19f863a4099c72cd688b14700
Heidism15 <heidism1508@gmail.com>
2024-02-15 10:43:32 +0100

    Enemigo
    Animaciones movimiento enemigo
    Ataque movimiento enemigo

--- Assets/Scripts/Enemigos/MovEnemigoSuelo.cs
+++ Assets/Scripts/Enemigos/MovEnemigoSuelo.cs
@@ -10,38 +10,61 @@ public class MovEnemigoSuelo : MonoBehaviour
     public Quaternion angulo;
     public float grado;
 
+    public GameObject target;
+
 
     // Start is called before the first frame update
     void Start()
     {
         ani = GetComponent<Animator>();
+        target = GameObject.Find("Personaje");
     }
 
     public void Comportamiento_Enemigo()
     {
-        Cronometro += 1 * Time.deltaTime;
-        if (Cronometro >= 4)
+        if (Vector3.Distance(transform.position, target.transform.position) > 7)
         {
-            rutina = Random.Range(0, 2);
-            Cronometro = 0;
+            ani.SetBool("Run", false);
+            Cronometro += 1 * Time.deltaTime;
+            if (Cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                Cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+                    ani.SetBool("Walk", false);
+                    break;
+
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 4f);
+                    transform.Translate(Vector3.forward * 3 * Time.deltaTime);
+                    ani.SetBool("Walk", true);
+                    break;
+
+            }
         }
-        switch (rutina)
+        else
         {
-            case 0:
-                ani.SetBool("Walk", false);
-                break;
-
-            case 1:
-                grado = Random.Range(0, 360);
-                angulo = Quaternion.Euler(0, grado, 0);
-                rutina++;
-                break;
-
-            case 2:
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                ani.SetBool("Walk", true);
-                break;
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+            ani.SetBool("Walk", false);
+            ani.SetBool("Run", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
         }
+    }
+
+    private void Update()
+    {
+        Comportamiento_Enemigo();
     }
 }
