5b19f800c60f717f6105fec18704198c1d586721
Heidism15 <heidism1508@gmail.com>
2024-02-19 17:15:17 +0100

    Menus y arreglos
    Ahora hay un menú de pausa funcional y las balas de la pistola desaparecen luego de cierto tiempo.

---
+++ Assets/Scripts/Menu/MenuPausa.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject ObjetoMenuPausa;
+    public bool Pausa = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjetoMenuPausa.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) 
+        { 
+            if (Pausa == false)
+            {
+                ObjetoMenuPausa.SetActive(true);
+                Pausa = true;
+
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+
+            }
+            else if (Pausa == true)
+            {
+                Resumir();
+            }
+        }
+    }
+
+    public void Resumir()
+    {
+        ObjetoMenuPausa.SetActive(false);
+        Pausa = false;
+
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
--- Assets/Scripts/Menu/StartGame.cs
+++ Assets/Scripts/Menu/StartGame.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEditor.ShaderData;
 
 public class StartGame : MonoBehaviour
 {
+   
+
     public void StartButton()
     {
         // Carga la escena principal del juego
         SceneManager.LoadScene("Nivel01");
+
     }
 }
