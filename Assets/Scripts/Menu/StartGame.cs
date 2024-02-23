55c010345e8b3b51d81d04329f6fd8c39ebc3196
Heidism15 <heidism1508@gmail.com>
2024-02-23 13:49:57 +0100

    Arreglos cosas
    Ahora el robot bola ataca y hace daño, y los canvas de pausa y derrota ya no estan descuadrados.

--- Assets/Scripts/Menu/StartGame.cs
+++ Assets/Scripts/Menu/StartGame.cs
@@ -6,10 +6,12 @@ using static UnityEditor.ShaderData;
 
 public class StartGame : MonoBehaviour
 {
-   
 
+    
     public void StartButton()
     {
+        Time.timeScale = 1;
+
         // Carga la escena principal del juego
         SceneManager.LoadScene("Nivel01");
 
